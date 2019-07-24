444fbab7bce51a0906601cb38d9ec95d33a72050
Horacio Duran <horacio.duran@gmail.com>
2019-07-24 16:16:49 -0700

    Initial PoC

---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+/*
+MIT License
+
+Copyright (c) 2019 Horacio Duran <horacio.duran@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/juju/gnuflag"
+	"github.com/pkg/errors"
+)
+
+var (
+	// ErrNothingToStaple sould be returned when we don't have arguments for the files
+	// or folders to staple to the binary.
+	ErrNothingToStaple = fmt.Errorf("there is nothing to staple to the binary")
+	// ErrNoBinary should be returned when the binary passed to stapple things to is
+	// either not a binary or not present.
+	ErrNoBinary = fmt.Errorf("the first argument is not a binary")
+)
+
+type configFields struct {
+	goBinary      string
+	target        string
+	stapleTargets []string
+}
+
+var config = configFields{}
+
+func init() {
+	gnuflag.CommandLine.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "%s [flags] <go-binary> <target> [fileOrFolder1 ...fileOrFolderN]:\n\n", os.Args[0])
+		gnuflag.PrintDefaults()
+	}
+
+	var err error
+	defer func() {
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Failures encountered: %v\n\n", err))
+			gnuflag.CommandLine.Usage()
+			os.Exit(1)
+		}
+	}()
+	err = gnuflag.CommandLine.Parse(true, os.Args[1:])
+	if err != nil {
+		err = errors.Wrap(err, "parsing comand arguments")
+		return
+	}
+
+	args := gnuflag.CommandLine.Args()
+	if len(args) < 3 {
+		err = ErrNothingToStaple
+		if len(args) < 1 {
+			err = errors.Wrapf(err, "missing some necessary arguments: %v", ErrNoBinary)
+		}
+		return
+	}
+
+	config.goBinary = args[0]
+	var fInfo os.FileInfo
+	fInfo, err = os.Stat(args[0])
+	if err != nil {
+		err = errors.Wrap(err, "performing stat in the binary")
+		return
+	}
+	if fInfo.IsDir() {
+		err = errors.Wrap(ErrNoBinary, "the passed go binary is a directory")
+		return
+	}
+	config.target = args[1]
+	fInfo, err = os.Stat(args[1])
+	if err == nil {
+		err = errors.Wrap(err, "the target file exists, for safety reasons we don't overwrite")
+		return
+	}
+	if fInfo.IsDir() {
+		err = errors.Wrap(ErrNoBinary, "the passed go binary is a directory")
+		return
+	}
+
+	// TODO: check this is a binary... for some definition of elf.
+	config.stapleTargets = make([]string, len(args)-2, len(args)-2)
+	for i := 2; i < len(args); i++ {
+		fInfo, err = os.Stat(args[i])
+		if err != nil {
+			err = errors.Wrap(err, "performing stat in one of the staple files")
+			return
+		}
+		if fInfo.IsDir() {
+			err = errors.Wrap(ErrNoBinary, "the passed go binary is a directory")
+			return
+		}
+		config.stapleTargets[i-2] = args[i]
+	}
+
+}
+
+func main() {
+	target, err := os.Create(config.target)
+	if err != nil {
+		fmt.Printf("cannot create the target file: %v\n", err)
+		os.Exit(1)
+	}
+	defer target.Close()
+	goBinary, err := os.Open(config.goBinary)
+	if err != nil {
+		fmt.Printf("cannot read source binary file: %v\n", err)
+		os.Exit(1)
+	}
+	defer goBinary.Close()
+	binarySize, err := io.Copy(target, goBinary)
+	if err != nil {
+		fmt.Printf("copying the go binary into the target: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("binary size: %d", binarySize)
+	writen, err := buildTar(config.stapleTargets, target)
+	if err != nil {
+		fmt.Printf("writing files into stappled go: %v", err)
+		os.Exit(1)
+	}
+	fmt.Printf("tar size: %d", writen)
+}
---
+++ staple.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"archive/tar"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+func buildTar(filesToInclude []string, tarFile io.Writer) (int64, error) {
+	tWriter := tar.NewWriter(tarFile)
+	return addToTar(filesToInclude, tWriter)
+}
+
+// buildTar will try to write the files passed into the passed ioWriter in tar format
+// if the writing fails some data might already be writte.
+func addToTar(filesToInclude []string, tWriter *tar.Writer) (int64, error) {
+	var contentsSize int64
+	tWriter.Flush()
+	for _, filePath := range filesToInclude {
+		fInfo, err := os.Stat(filePath)
+		if err != nil {
+			return 0, errors.Wrap(err, "accessing file to include in stappled file")
+		}
+		// Header
+		// for now we just dereference symlinks
+		hdr, err := tar.FileInfoHeader(fInfo, "")
+		if err != nil {
+			return 0, errors.Wrap(err, "creating header for file")
+		}
+		hdr.Name = filePath
+		err = tWriter.WriteHeader(hdr)
+		if err != nil {
+			return 0, errors.Wrapf(err, "writing header information for %s", filePath)
+		}
+		// File
+		if !fInfo.IsDir() {
+			contents, err := ioutil.ReadFile(filePath)
+			if err != nil {
+				return 0, errors.Wrap(err, "reading file to add into tar")
+			}
+			writen, err := tWriter.Write(contents)
+			if err != nil {
+				return 0, errors.Wrap(err, "writing file contents into tar")
+			}
+
+			// Fixme: this is wrong?
+			contentsSize += int64(writen)
+			continue
+		}
+		dirContents, err := filepath.Glob(filepath.Join(filePath, "*"))
+		if err != nil {
+			return 0, errors.Wrapf(err, "trying to read the contents of %s", filePath)
+		}
+		for i := range dirContents {
+			dirContents[i] = filepath.Join(filePath, dirContents[i])
+		}
+		writen, err := addToTar(dirContents, tWriter)
+		if err != nil {
+			return 0, errors.Wrapf(err, "adding to tar the contents of %s", filePath)
+		}
+		contentsSize += writen
+	}
+
+	return contentsSize, tWriter.Flush()
+}
